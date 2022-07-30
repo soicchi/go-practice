7514ca7d4417d3f6eb94caa27224a0a512809974
soichiro-nishizono <soicchi@icloud.com>
2022-07-30 14:05:33 +0900

    study calculate operation

---
+++ study/operation.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+		// 加算
+		fmt.Println(1 + 2)
+		// 減算
+		fmt.Println(5 - 1)
+		// 乗算
+		fmt.Println(5 * 3)
+		// 除算
+		fmt.Println(60 / 3)
+		// 余剰
+		fmt.Println(34 % 5)
+		// 文字列の結合
+		fmt.Println("ABC" + "DFG")
+		// 短縮系
+		num := 100
+		num += 20  // num = num + 20
+		num++  // num + 1
+		num--  // num - 1
+}
