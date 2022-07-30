57c9d406db3a8f36e53bf567eb77b660ed86fc68
soichiro-nishizono <soicchi@icloud.com>
2022-07-30 19:50:31 +0900

    study type assertion

--- study/control_syntax.go
+++ study/control_syntax.go
@@ -23,4 +23,22 @@ func main() {
 				fmt.Println(i)
 				i++
 		}
+
+		// swicth分もあるが省略
+
+
+		// 型アサーション
+		var anything interface{} = 3
+		result := anything.(int) + 2
+		fmt.Println(result)
+		/*
+				変数 := i.(T)
+						interface型に代入された値の型を復元してくれる
+						ただし、代入している型に一致していないとpanicを引き起こす
+						ex) var sample interface{} = 1
+								sample.(float64)  -> もともとはint型なのでfloat型には復元できない
+						変数を二つにすると2つ目の変数に復元できれば、true
+						復元できなければ、falseになりpanicにならずに処理が進む
+		*/
+
 }
