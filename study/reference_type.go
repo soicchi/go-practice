c377846d1d97adae0286371b481c1d55c442df15
soichiro-nishizono <soicchi@icloud.com>
2022-07-30 20:53:28 +0900

    study each methods of slice

--- study/reference_type.go
+++ study/reference_type.go
@@ -15,4 +15,26 @@ func main() {
 
 		fmt.Println(sl[2])
 		fmt.Println(sl[1:])
+
+
+		// append
+		sl3 := []int{1, 2, 3}
+		sl3 = append(sl3, 4)
+		fmt.Println(sl3)
+
+		// append(複数入れることもできる)
+		sl3 = append(sl3, 5, 6, 7)
+		fmt.Println(sl3)
+
+		// make(指定した要素数分の初期値をスライスに格納)
+		sl4 := make([]int, 5)
+		fmt.Println(sl4)
+
+		// len
+		sl5 := []int{1, 2}
+		fmt.Println(len(sl5))
+
+		// cap(スライスの容量を確認する)
+		sl6 := []int{1, 6, 9}
+		fmt.Println(cap(sl6))
 }
